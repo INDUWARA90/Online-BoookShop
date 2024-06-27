62f372949da9ecc5c2784030dc7153cbaf608bd8
INDUWARA90 <induwaravishwakantha90@gmail.com>
2024-06-27 07:24:49 +0530

    create Cart.html

--- script/products.js
+++ script/products.js
@@ -1,47 +1,55 @@
 
 //=========Data Array(Books)===============
-const Products=[{
+export const Products=[{
+    id:'B0001',
     image:'asset/Book01.jpg',
     name:`Harry Potter and the Philosopher's Stone`,
     description:`Harry Potter and the Philosopher's Stone is a fantasy novel written by British author
               J. K. Rowling. The first novel in the Harry Potter series`
 },{
+    id:'B0002',
     image:'asset/Book02.jpg',
     name:`Harry Potter and the Chamber of Secrets`,
     description:`Harry Potter and the Chamber of Secrets is a fantasy novel written by British author J.
               K. Rowling and the second novel in the Harry Potter series. `
 
 },{
+    id:'B0003',
     image:'asset/Book03.jpg',
     name:`Harry Potter and the Prisoner of Azkaban`,
     description:`Harry Potter and the Prisoner of Azkaban is a fantasy novel written by British author
               J. K. Rowling and is the third in the Harry Potter series. `
 
 },{
+    id:'B0004',
     image:'asset/Book04.jpg',
     name:`Harry Potter and the Goblet of Fire`,
     description:`Harry Potter and the Goblet of Fire is a fantasy novel written by British author J. K.
               Rowling and the fourth novel in the Harry Potter series. `
 
 },{
+    id:'B0005',
     image:'asset/Book05.jpg',
     name:`Harry Potter and the Order of the Phoenix`,
     description:`Harry Potter and the Order of the Phoenix is a fantasy novel written by British author
               J. K. Rowling and the fifth novel in the Harry Potter series.`
 
 },{
+    id:'B0006',
     image:'asset/Book06.jpg',
     name:`Harry Potter and the Half-Blood Prince`,
     description:`Harry Potter and the Half-Blood Prince is a fantasy novel written by British author J.
               K. Rowling and the sixth and penultimate novel in the Harry Potter series. `
 
 },{
+    id:'B0007',
     image:'asset/Book07.jpg',
     name:`Harry Potter and the Deathly Hallows `,
     description:`Harry Potter and the Deathly Hallows is a fantasy novel written by British author J. K.
               Rowling and the seventh and final novel in the Harry Potter series. `
 
 },{
+    id:'B0008',
     image:'asset/Book08.jpg',
     name:`Harry Potter and the Cursed Child`,
     description:`Cursed Child is a play written by Jack Thorne from an original story written by J. K.
--- script/script.js
+++ script/script.js
@@ -1,21 +1,58 @@
+import {Products} from "./products.js"
+import {Cart} from "./Mycart.js"
+
+
 
 //=========to retrive data from arry to format===============
-let getHTML='';
+let getHTML = '';
 
-Products.forEach((product)=>{
-    getHTML+=`
+Products.forEach((product) => {
+  getHTML += `
         <div class="col-sm-12 ms-5 mt-3  col-lg-2 m-lg-5   col-md-3 m-md-4">
         <div class="card" style="width: 14rem;">
           <img src="${product.image}" class="card-img-top asset-size" alt="...">
           <div class="card-body">
-            <h5 class="card-title fw-bold  fText">${product.name}</h5>
+            <h5 class="card-title fw-bold fText">${product.name}</h5>
             <p class="card-text">${product.description}</p>
-            <a href="#" class="btn btn-primary">Download</a>
+            <a href="#" class="btn btn-primary js-download-button data-product-id="${product.id}" data-product-id="${product.id}" data-product-name="${product.name}" ">Download</a>
           </div>
         </div>
       </div>
     `
 });
-
 //===============give the data to html page=============
-document.querySelector(".js-products").innerHTML=getHTML;
+document.querySelector(".js-products").innerHTML = getHTML;
+
+
+
+//==================add product to cart=====================
+document.querySelectorAll('.js-download-button')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      let getName, getId = ``;
+
+      getId = button.dataset.productId;
+      getName = button.dataset.productName;
+
+      let isExist = ``;
+      Cart.forEach((iteam) => {
+        if (getId === iteam.id) {
+          isExist = iteam;
+        }
+      });
+
+      if (isExist) {
+        isExist.quantity++;
+      } else {
+        Cart.push({
+          id: getId,
+          name: getName,
+          quantity: 1
+
+        });
+      }
+      console.log(Cart);
+    });
+  });
+
+//=====================================
